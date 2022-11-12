155a018e9cb4d622a2aa4117b9eb31fa40d34d57
Denys Demianyk <ddemianyk@sphera.com>
2022-11-12 09:46:24 +0100

    Initial commit

---
+++ src/main/java/org/examddemianyk/words/wordrelationship/WordRelationshipApplication.java
@@ -0,0 +1,13 @@
+package org.examddemianyk.words.wordrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordRelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/examddemianyk/words/wordrelationship/WordRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.examddemianyk.words.wordrelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
