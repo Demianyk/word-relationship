5d1bbae7c8b8d5fc2dec52f4ffe893bcd5246186
Denys Demianyk <ddemianyk@sphera.com>
2022-11-12 12:10:55 +0100

    Added validation which allows entering only letters and spaces in W1 and W2 values (task 6)

--- src/main/java/org/ddemianyk/words/wordrelationship/dto/RelationshipDTO.java
+++ src/main/java/org/ddemianyk/words/wordrelationship/dto/RelationshipDTO.java
@@ -5,14 +5,17 @@ import lombok.Setter;
 import org.ddemianyk.words.wordrelationship.model.RelType;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class RelationshipDTO {
 
+    @Pattern(regexp = "[a-zA-z\\s]*", message = "Only letters and spaces are allowed for W1 values")
     @NotNull(message = "W1 can't be null")
     private String w1;
 
+    @Pattern(regexp = "[a-zA-z\\s]*", message = "Only letters and spaces are allowed for W2 values")
     @NotNull(message = "W2 can't be null")
     private String w2;
 
