e06328aa7712e349d328bd6d1c651202987e2959
Denys Demianyk <ddemianyk@sphera.com>
2022-11-12 11:00:51 +0100

    Changed http status to CREATED for add relationship endpoint

--- src/main/java/org/examddemianyk/words/wordrelationship/controller/RelationshipController.java
+++ src/main/java/org/examddemianyk/words/wordrelationship/controller/RelationshipController.java
@@ -3,11 +3,13 @@ package org.examddemianyk.words.wordrelationship.controller;
 import lombok.RequiredArgsConstructor;
 import org.examddemianyk.words.wordrelationship.dto.RelationshipDTO;
 import org.examddemianyk.words.wordrelationship.service.RelationshipService;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -22,6 +24,7 @@ public class RelationshipController {
     private final RelationshipService relationshipService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void save(@Valid @RequestBody RelationshipDTO relationshipDTO) {
         relationshipService.save(relationshipDTO);
     }
